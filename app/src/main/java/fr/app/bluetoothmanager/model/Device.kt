48e2d16cb55efaa9695ab22ba23a615e6176e4e9
MikeDel88 <delamarre.michael@gmail.com>
2026-01-20 15:50:37 +0100

    Ajout de paramètres sur la connectivité et le type.

--- app/src/main/java/fr/app/bluetoothmanager/model/Device.kt
+++ app/src/main/java/fr/app/bluetoothmanager/model/Device.kt
@@ -1,6 +1,14 @@
 package fr.app.bluetoothmanager.model
 
+enum class DeviceType {
+    CLASSIC,
+    BLE
+}
+
 data class Device(
     val name: String?,
-    val address: String
+    val address: String,
+    val bonded: Boolean,
+    val connected: Boolean,
+    val type: DeviceType
 )
